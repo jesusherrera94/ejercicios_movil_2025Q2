c3491e74533c1e1eacffad111078cb7b76349871
Jesus Herrera <jesusherrera1594@gmail.com>
2025-05-02 20:58:19 -0600

    implement BindingObservers

--- states/lib/counter.dart
+++ states/lib/counter.dart
@@ -3,12 +3,17 @@ import 'dart:async';
 import 'dart:io';
 
 class Counter extends StatefulWidget {
+
   const Counter({super.key});
- @override
- State<Counter> createState() => _CounterState(); 
+
+  @override
+  State<Counter> createState() => _CounterState();
+
 }
 
-class _CounterState extends State<Counter> {
+
+class _CounterState extends State<Counter> with WidgetsBindingObserver {
+
   int _counter = 0;
 
   late Timer _timer;
@@ -16,33 +21,50 @@ class _CounterState extends State<Counter> {
   @override
   void initState() {
     _startCountDown();
+    WidgetsBinding.instance.addObserver(this);
     super.initState();
   }
 
+
   void _startCountDown() {
     _timer = Timer.periodic(Duration(seconds: 1), (_) {
-      if (_counter <=0 ) {
-        setState(() {
-          _counter = 10;
-        });
+      if(_counter <= 0) {
+        _counter = 10; 
       } else {
-        setState(() {
-          _counter--;
-        });
+        _counter--;
       }
+      setState(() {});
+      print('=====> $_counter');
     });
+  }
+
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState state) {
+    switch(state) {
+      case AppLifecycleState.paused:
+        print('User exits app!');
+        _timer.cancel();
+        break;
+      case AppLifecycleState.resumed:
+        print('User returned to app');
+        _startCountDown();
+        break;
+      default:
+        print('No State detected');
+    }
   }
 
   @override
   void dispose() {
     _timer.cancel();
+     WidgetsBinding.instance.removeObserver(this);
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Text('$_counter', style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold)),
+      child: Text('$_counter', style: TextStyle( color: Colors.white, fontWeight: FontWeight.bold),),
     );
   }
 
