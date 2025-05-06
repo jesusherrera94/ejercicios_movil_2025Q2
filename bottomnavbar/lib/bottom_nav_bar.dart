93090f96e8b60cadf82b21c37bcdf1d9389bc4be
Jesus Herrera <jesusherrera1594@gmail.com>
2025-05-05 21:00:40 -0600

    Update bottom_nav_bar.dart

--- bottomnavbar/lib/bottom_nav_bar.dart
+++ bottomnavbar/lib/bottom_nav_bar.dart
@@ -3,53 +3,48 @@ import 'screens/home.dart';
 import 'screens/profile.dart';
 
 class MyBottomNavigationBar extends StatefulWidget {
-
+  
   @override
-  State<MyBottomNavigationBar> createState() => _MyBottomNavigationBar();
-
+  State<MyBottomNavigationBar> createState() => _MyBottomNavigationBarState(); 
 }
 
-
-class _MyBottomNavigationBar extends State<MyBottomNavigationBar> {
+class _MyBottomNavigationBarState extends State<MyBottomNavigationBar> {
 
   int _selectedIndex = 0;
 
   final List<Map<String, dynamic>> _screens = [
-     {
+    {
       "title": 'Home',
-      "screen": HomeScreen(),
+      "screen": HomeScreen()
     },
     {
       "title": 'Profile',
-      "screen": ProfileScreen(),
+      "screen": ProfileScreen()
     }
   ];
 
-
-
-  void _selectNewIndex(int newIndex) {
+  void _selectedNewIndex (int newIndex) {
     setState(() {
       _selectedIndex = newIndex;
     });
   }
-
-  // TODO: implement conditional action button!
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(_screens[_selectedIndex]["title"]),
-        automaticallyImplyLeading: false,
+        automaticallyImplyLeading: false, // esto para el futuro!
       ),
-      body: _screens[_selectedIndex]["screen"],
+      body: _screens[_selectedIndex]['screen'],
+      floatingActionButton: FloatingActionButton(onPressed: () {}, child: Icon(Icons.add),),
       bottomNavigationBar: BottomNavigationBar(items: [
-        BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
-        BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Profile')
+        BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'), // 0
+        BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Profile'), // 1
       ],
-        currentIndex: _selectedIndex,
-        selectedItemColor: Colors.blueAccent,
-        onTap: _selectNewIndex,
+      currentIndex: _selectedIndex,
+      selectedItemColor: Colors.blueAccent,
+      onTap: _selectedNewIndex,
       ),
     );
   }
