1a96a697682bb5ae2620ba3bb38248f30fe2dc96
Jesus Herrera <jesusherrera1594@gmail.com>
2025-05-05 21:01:23 -0600

    Update tab_controller.dart

--- tabcontroller/lib/tab_controller.dart
+++ tabcontroller/lib/tab_controller.dart
@@ -3,22 +3,25 @@ import 'screens/home.dart';
 import 'screens/profile.dart';
 
 class TabControllerScreen extends StatelessWidget {
+
   const TabControllerScreen({super.key});
+
   @override
   Widget build(BuildContext context) {
     return DefaultTabController(length: 2, 
     child: Scaffold(
       appBar: AppBar(
-        bottom: const TabBar(tabs: [
-          Tab( icon: Icon(Icons.home),),
-          Tab( icon: Icon(Icons.person),)
-        ]),
-        title: const Text('Tab Controller app'),
+        title: const Text('My super tab app!'),
+        bottom: const TabBar( tabs: [
+        Tab(icon: Icon(Icons.home),),
+        Tab(icon: Icon(Icons.person),),
+      ],),
       ),
       body: const TabBarView(children: [
-          HomeScreen(),
-          ProfileScreen()
+        HomeScreen(),
+        ProfileScreen()
       ]),
     ));
   }
+  
 }
