8fe9c57e1324f1fe8f583c33db1aabaad6ff1276
Jesus Herrera <jesusherrera1594@gmail.com>
2025-05-05 21:02:08 -0600

    Update drawer.dart

--- menudrawer/lib/drawer.dart
+++ menudrawer/lib/drawer.dart
@@ -4,67 +4,68 @@ import 'screens/profile.dart';
 
 
 class MenuDrawer extends StatefulWidget {
-  const MenuDrawer({super.key});
+
   @override
   State<MenuDrawer> createState() => _MenuDrawerState();
+  
 }
 
 class _MenuDrawerState extends State<MenuDrawer> {
 
   int _selectedIndex = 0;
 
-  static const List<Widget> _screens = [
+  final List<Widget> _screens = [
     HomeScreen(),
     ProfileScreen()
   ];
 
-    void _onTapMenuItem(int newIndex) {
-        setState(() {
-          _selectedIndex = newIndex;
-        });
-    }
+  void _onTapMenuItem(int newIndex) {
+    setState(() {
+      _selectedIndex = newIndex;
+    });
+  }
+
+
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar( 
-        title: Text('My app title!'),
+      appBar: AppBar(
+        title: Text('My menu drawer!'),
         leading: Builder(builder: (context) {
           return IconButton(onPressed: () {
-            Scaffold.of(context).openDrawer();
-          }, icon: const Icon(Icons.menu));
+            Scaffold.of(context).openDrawer(); // abrir el menu
+          }, icon: Icon(Icons.menu));
         }),
-        ),
+      ),
       body: _screens[_selectedIndex],
       drawer: Drawer(
         child: ListView(
-          padding: EdgeInsets.zero,
+          padding: EdgeInsets.zero, // removiendo padding
           children: [
-            const DrawerHeader(
-              decoration: BoxDecoration(color: Colors.blueAccent),
-              child: Text('Drawer Header!')),
-              ListTile(
-                title: const Text('Home'),
-                selected: _selectedIndex == 0,
-                onTap: () {
-                  _onTapMenuItem(0);
-                  // closes the drawer!
-                  Navigator.pop(context);
-                },
-              ),
-              ListTile(
-                title: const Text('Profile'),
-                selected: _selectedIndex == 1,
-                onTap: () {
-                  _onTapMenuItem(1);
-                  // closes the drawer!
-                  Navigator.pop(context);
-                },
-              )
+            DrawerHeader(child: Text('Drawer Header!'),
+            decoration: BoxDecoration(color: Colors.blueAccent),
+            ),
+            ListTile(
+              title: Text('Home'),
+              selected: _selectedIndex == 0,
+              onTap: () {
+                _onTapMenuItem(0);
+                Navigator.pop(context); // cerrar el menu drawer
+              },
+            ),
+            ListTile(
+              title: Text('Profile'),
+              selected: _selectedIndex == 1,
+              onTap: () {
+                _onTapMenuItem(1);
+                Navigator.pop(context); // cerrar el menu drawer
+              },
+            )
           ],
         ),
       ),
     );
-
   }
+  
 }
