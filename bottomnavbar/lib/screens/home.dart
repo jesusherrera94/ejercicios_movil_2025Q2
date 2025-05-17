e5bfa3b0d5deb213c6a8052e5c7d4c1b33ba29bc
Jesus Herrera <jesusherrera1594@gmail.com>
2025-05-16 20:55:04 -0600

    Update home.dart with class code

--- bottomnavbar/lib/screens/home.dart
+++ bottomnavbar/lib/screens/home.dart
@@ -10,8 +10,8 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   
-  // temporal final!
   Period? _selectedFilter;
+  // temporal final!
   final List<Subscription> _subscriptions = [
     Subscription(
       id: '1',
@@ -57,77 +57,78 @@ class _HomeScreenState extends State<HomeScreen> {
 
   List<Subscription> _filteredSubscriptions = [];
 
-
   List<Widget> _renderItems() {
   List<Widget> subscriptionWidget = [];
-  if (_selectedFilter == null) {
-    for(final subscription in _subscriptions) {
-      subscriptionWidget.add(SubscriptionItem(subscriptionElement: subscription,));
+
+    if(_selectedFilter == null) {
+      // funciona cuando inicia el app y cuando se restaure el filtro
+      // aka. estado inicial
+        for(final subscription in _subscriptions) {
+         subscriptionWidget.add(SubscriptionItem(subscriptionElement: subscription,));
+        }
+      setState(() {
+        _filteredSubscriptions = _subscriptions;
+      });
+    } else {
+      _filteredSubscriptions = _subscriptions.where((subs) {
+        return subs.renovationCycle == _selectedFilter;
+      }).toList();
+
+      for(final subscription in _filteredSubscriptions) {
+        subscriptionWidget.add(SubscriptionItem(subscriptionElement: subscription,));
+      }
+      setState(() {});
     }
-    setState(() {
-      _filteredSubscriptions = _subscriptions;
-    });
-  }
-  else {
-    _filteredSubscriptions = _subscriptions.where((subscription) {
-      return subscription.renovationCycle == _selectedFilter;
-    }).toList();
-    for(final subscription in _filteredSubscriptions) {
-      subscriptionWidget.add(SubscriptionItem(subscriptionElement: subscription,));
-    }
-    setState(() {});
-  }
-  
   return subscriptionWidget;
 }
 
-
-  void _onApplyFilter(Period? period) {
-    setState(() {
-      _selectedFilter = period;
-    });
-  }
+void _selectPeriod(Period? newPeriod) {
+  setState(() {
+    _selectedFilter = newPeriod;
+  });
+}
 
   @override
   Widget build(BuildContext context) {
     return Column(
       children: [
         Row(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: [
-              IconButton(onPressed: () {
-                _onApplyFilter(null);
-              }, icon: const Icon(Icons.cancel)),
-              ElevatedButton(onPressed: () {
-                _onApplyFilter(Period.DAILY);
-              },
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            if (_selectedFilter != null)
+            IconButton(onPressed: () {
+              _selectPeriod(null);
+            }, icon: Icon(Icons.cancel)),
+            ElevatedButton(onPressed: () {
+              if(_selectedFilter == Period.DAILY) {
+                _selectPeriod(null);
+              } else {
+                _selectPeriod(Period.DAILY);
+              }
+            }, 
               style: ElevatedButton.styleFrom(
-                      backgroundColor: _selectedFilter == Period.DAILY
-                          ? Colors.blueAccent 
-                          : Colors.grey,
-                    ), 
-              child: const Text("Daily"),
-              
+                backgroundColor: _selectedFilter == Period.DAILY ?
+                  Colors.blueAccent : Colors.grey 
               ),
-              ElevatedButton(onPressed: () {
-                _onApplyFilter(Period.MONTHLY);
-              }, 
-              style: ElevatedButton.styleFrom(
-                      backgroundColor: _selectedFilter == Period.MONTHLY
-                          ? Colors.blueAccent
-                          : Colors.grey,
-                    ), 
-              child: const Text("Monthly")),
-              ElevatedButton(onPressed: () {
-               _onApplyFilter(Period.YEARLY);
-              }, 
-              style: ElevatedButton.styleFrom(
-                      backgroundColor: _selectedFilter == Period.YEARLY
-                          ? Colors.blueAccent
-                          : Colors.grey,
-                    ), 
-              child: const Text("Yearly")),
-            ],
+            child: const Text("Daily"),
+            ),
+            ElevatedButton(onPressed: () {
+              _selectPeriod(Period.MONTHLY);
+            }, 
+            style: ElevatedButton.styleFrom(
+                backgroundColor: _selectedFilter == Period.MONTHLY ?
+                  Colors.blueAccent : Colors.grey 
+              ),
+            child: const Text("Monthly")),
+            ElevatedButton(onPressed: () {
+              _selectPeriod(Period.YEARLY);
+            },
+            style: ElevatedButton.styleFrom(
+                backgroundColor: _selectedFilter == Period.YEARLY ?
+                  Colors.blueAccent : Colors.grey 
+              ),
+             child: const Text("Yearly"))
+          ],
         ),
         Expanded(
       child:ListView(
