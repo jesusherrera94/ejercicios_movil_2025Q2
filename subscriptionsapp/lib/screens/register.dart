524df870222773ed7ddfcedea713f101c4ddd47a
Jesus Herrera <jesusherrera1594@gmail.com>
2025-05-30 21:01:46 -0600

    Update register.dart

--- subscriptionsapp/lib/screens/register.dart
+++ subscriptionsapp/lib/screens/register.dart
@@ -1,8 +1,9 @@
-import 'dart:convert';
+import 'dart:convert' as convert;
 import 'package:flutter/material.dart';
 import '../models/user.dart';
 import '../adapters/dio_adapter.dart';
 import '../adapters/local_storage.dart';
+
 
 class Register extends StatefulWidget {
   const Register({super.key});
@@ -13,19 +14,19 @@ class Register extends StatefulWidget {
 
 class _RegisterState extends State<Register> {
 
-  // form key
+
   final _formKey = GlobalKey<FormState>();
   DioAdapter _dioAdapter = DioAdapter();
   LocalStorage _localStorage = LocalStorage();
+  bool _isLoading = false;
 
-  // User data
-  final _user = User(
+
+  User _user = User(
     username: '',
     fullname: '',
     email: '',
     password: '',
-    principalInterest: '',
-  );
+    principalInterest: '');
 
   void _displaySnackbar(BuildContext context, String message) {
     SnackBar snackBar = SnackBar(content: Text(message),
@@ -34,92 +35,103 @@ class _RegisterState extends State<Register> {
       }),
     );
     ScaffoldMessenger.of(context).showSnackBar(snackBar);
-    Navigator.pushNamed(context, 'app-controller');
+   Navigator.pushNamed(context, 'app-controller');
   }
 
-  void _onSubmit(BuildContext context) async {
-    if (_formKey.currentState!.validate()) {
+  void onSubmit() async {
+    if(_formKey.currentState!.validate()) {
       _formKey.currentState!.save();
+      setState(() {
+        _isLoading = true;
+      });
       try {
-        _user.profilePicture = "https://example.com/profile2.jpg";
+        _user.profilePicture = 'https://example.com/profile2.jpg';
+
         dynamic response = await _dioAdapter.postRequest('https://subscriptions-be.vercel.app/api/users', _user.toMap());
-        User newUser = User.fromMap(response['user']);
-        _localStorage.setUserData('user', newUser.toMapString());
-        _localStorage.setLoginStatus(true);
-        // testing the local storage
-        String userStr = await _localStorage.getUserData('user');
-        dynamic userMap = jsonDecode(userStr);
+        setState(() {
+          _isLoading = false;
+        });
+        User newUser = User.fromMap(response["user"]);
+        await _localStorage.setUserData('user', newUser.toMapString());
+        await _localStorage.setLoginStatus(true);
+
+        // Testing the localStorage data
+        String userString = await _localStorage.getUserData('user');
+        dynamic userMap = convert.jsonDecode(userString);
         _displaySnackbar(context, 'User registered successfully: ${userMap['username']}');
-      } catch (e) {
-        print('An error has occurred!: $e');
-        _displaySnackbar(context, 'An error has occurred trying to register the user!');
-      }    
+
+      } catch(e) {
+        print('An error ocurred trying to create user: $e');
+        _displaySnackbar(context, 'An error has ocurred trying to create user!');
+        setState(() {
+        _isLoading = false;
+      });
+      }
     }
   }
 
- @override
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('User Registration'),
+        title: Text('User Registration'),
       ),
       body: SingleChildScrollView(
         child: Padding(
           padding: const EdgeInsets.all(16.0),
           child: Form(
-              key: _formKey, // Form key
-              child: Column(
-                children: [
-                  // TextFields
-                  TextFormField(
-                    decoration: const InputDecoration(labelText: 'Username'),
-                    validator: (value) {
-                      if (value == null || value.isEmpty) {
-                        return 'Please enter a username';
-                      }
-                      return null;
-                    },
-                    onSaved: (value) => _user.username = value!,
-                  ),
-                  TextFormField(
-                    decoration: const InputDecoration(labelText: 'Full Name'),
-                    validator: (value) {
-                      if (value == null || value.isEmpty) {
-                        return 'Please enter your full name';
-                      }
-                      return null;
-                    },
-                    onSaved: (value) => _user.fullname = value!,
-                  ),
-                  TextFormField(
-                    decoration: const InputDecoration(labelText: 'Email'),
-                    validator: (value) {
-                      if (value == null || value.isEmpty) {
-                        return 'Please enter your email';
-                      }
-                      if (!value.contains('@')) {
-                        return 'Please enter a valid email';
-                      }
-                      return null;
-                    },
-                    onSaved: (value) => _user.email = value!,
-                  ),
-                  TextFormField(
-                    decoration: const InputDecoration(labelText: 'Password'),
-                    obscureText: true,
-                    validator: (value) {
-                      if (value == null || value.isEmpty) {
-                        return 'Please enter a password';
-                      }
-                      if (value.length < 6) {
-                        return 'Password must be at least 6 characters';
-                      }
-                      return null;
-                    },
-                    onSaved: (value) => _user.password = value!,
-                  ),
-                  const SizedBox(height: 20),
-                  TextFormField(
+            key: _formKey,
+            child: 
+            Column(
+              children: [
+                TextFormField(
+                  decoration: const InputDecoration(labelText: 'Username'),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter the username';
+                    }
+                    return null;
+                  },
+                  onSaved: (value) => _user.username = value!,
+                ),
+                TextFormField(
+                  decoration: const InputDecoration(labelText: 'Full name'),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter the Full name';
+                    }
+                    return null;
+                  },
+                  onSaved: (value) => _user.fullname = value!,
+                ),
+                TextFormField(
+                  decoration: const InputDecoration(labelText: 'Email'),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter the Email';
+                    }
+                    if(!value.contains("@")){
+                      return 'Please enter a valid Email';
+                    }
+                    return null;
+                  },
+                  onSaved: (value) => _user.email = value!,
+                ),
+                TextFormField(
+                  decoration: const InputDecoration(labelText: 'Password'),
+                  obscureText: true,
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter the Password';
+                    }
+                    if (value.length < 6) {
+                      return 'Password must be at least 6 characters long';
+                    }
+                    return null;
+                  },
+                  onSaved: (value) => _user.password = value!,
+                ),
+                TextFormField(
                     decoration: const InputDecoration(labelText: 'Your interest'),
                     validator: (value) {
                       if (value == null || value.isEmpty) {
@@ -129,23 +141,22 @@ class _RegisterState extends State<Register> {
                     },
                     onSaved: (value) => _user.principalInterest = value!,
                   ),
-
-                  // Submit button
-                  const SizedBox(height: 30),
+                  const SizedBox(height: 30,),
                   ElevatedButton(
-                    onPressed: (){
-                      _onSubmit(context);
-                      }, // Callback function
-                    child: const Text('Register'),
-                  ),
-                ],
-              ),
+                    
+                    onPressed: () {
+                      if (!_isLoading){
+                        onSubmit();
+                      }
+                  }, child: _isLoading ? CircularProgressIndicator() : Text("Register"),
+                  )
+
+              ],
+            )
             ),
-          ),
+        
         ),
+      )
     );
   }
-
-
-
 }
