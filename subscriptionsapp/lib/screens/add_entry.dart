9aff437bf16fc02b4f9e7a941fc40865b5c29a6f
Jesus Herrera <jesusherrera1594@gmail.com>
2025-06-04 21:02:23 -0600

    add dropdown

--- subscriptionsapp/lib/screens/add_entry.dart
+++ subscriptionsapp/lib/screens/add_entry.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import '../widgets/wave_button.dart';
+import '../models/subscription.dart';
+
 class AddEndry extends StatefulWidget {
   const AddEndry({super.key});
 
@@ -8,24 +10,135 @@ class AddEndry extends StatefulWidget {
 }
 
 class _AddEndryState extends State<AddEndry> {
+  final _formKey = GlobalKey<FormState>();
+  bool _isLoading = false;
 
-  void _onCreateSubscription() {
-    print('subscription created!!!!!!!!!!!!!!!');
+  Subscription _newSubscription = Subscription(
+    platformName: '',
+    renovationDate: 0,
+    renovationCycle: Period.NONE,
+    charge: 0.0,
+  );
+
+  void _onCreateSubscription() async {
+    if (_formKey.currentState!.validate()) {
+      _formKey.currentState!.save();
+      setState(() {
+        _isLoading = true;
+      });
+      print('subscription created!!!!!!!!!!!!!!! ${_newSubscription.toMap()}');
+      setState(() {
+        _isLoading = false;
+      });
+    }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar( title: Text("Add Subscription"),),
+      appBar: AppBar(title: Text("Add Subscription")),
       body: Center(
-      child: Column(
-        children: [
-          Text('Add entry here!'),
-          SizedBox(height: 30,),
-          WaveButton(onPressed: _onCreateSubscription, child: const Text('Add subscription'))
-        ],
+        child: Padding(
+          padding: EdgeInsets.all(20),
+          child: SizedBox(
+            height: MediaQuery.of(context).size.height * 0.5,
+            child: Card(
+              child: Form(
+                key: _formKey,
+                child: Padding(
+                  padding: EdgeInsets.all(10),
+                  child: Column(
+                    children: [
+                      Text("Add the subscription info here", style: TextStyle(fontSize: 18),),
+                      SizedBox(height: 10,),
+                      TextFormField(
+                        decoration: const InputDecoration(
+                          labelText: 'Platform',
+                        ),
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Please enter the Platform';
+                          }
+                          return null;
+                        },
+                        onSaved:
+                            (value) => _newSubscription.platformName = value!,
+                      ),
+                      TextFormField(
+                        decoration: const InputDecoration(
+                          labelText: 'Renovation Date',
+                        ),
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Please enter the Renovation date';
+                          }
+                          return null;
+                        },
+                        onSaved:
+                            (value) =>
+                                _newSubscription.renovationDate = int.parse(
+                                  value!,
+                                ),
+                      ),
+                      // TODO add dropdown!
+                      DropdownButtonFormField<Period>(
+                        decoration: const InputDecoration(
+                          labelText: "Renovation Cycle",
+                        ),
+                        items:
+                            Period.values.map((Period period) {
+                              return DropdownMenuItem<Period>(
+                                value: period,
+                                child: Text(period.name),
+                              );
+                            }).toList(),
+                        onChanged: (value) {
+                          if (value != null) {
+                            _newSubscription.renovationCycle = value;
+                          }
+                        },
+                        validator: (value) {
+                          if (value == null || value == Period.NONE) {
+                            return 'Please select a valid renovation cycle';
+                          }
+                          return null;
+                        },
+                      ),
+                      TextFormField(
+                        decoration: const InputDecoration(labelText: 'Charge'),
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Please enter the Charge';
+                          }
+                          if (double.tryParse(value) == null) {
+                            return 'Please enter a valid number';
+                          }
+                          return null;
+                        },
+                        onSaved:
+                            (value) =>
+                                _newSubscription.charge = double.parse(value!),
+                      ),
+                      const SizedBox(height: 30),
+                      WaveButton(
+                        onPressed: () {
+                          if (!_isLoading) {
+                            _onCreateSubscription();
+                          }
+                        },
+                        child:
+                            _isLoading
+                                ? CircularProgressIndicator()
+                                : Text("Register"),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ),
       ),
-    ),
     );
   }
 }
