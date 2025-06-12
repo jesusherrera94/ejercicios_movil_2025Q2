aeaeb8eccf47abc113ad272abc519fdaa78abe7d
Jesus Herrera <jesusherrera1594@gmail.com>
2025-06-11 20:56:10 -0600

    Update subscription_item.dart

--- subscriptionsapp/lib/widgets/subscription_item.dart
+++ subscriptionsapp/lib/widgets/subscription_item.dart
@@ -2,8 +2,7 @@ import 'package:flutter/material.dart';
 import '../models/subscription.dart';
 import '../utils/time_utils.dart';
 import '../adapters/db.dart';
-import '../state/subscriptions_state.dart';
-
+import '../state/subscription_state.dart';
 
 class SubscriptionItem extends StatelessWidget {
   final Subscription subscriptionElement;
@@ -12,26 +11,36 @@ class SubscriptionItem extends StatelessWidget {
 
   Future<void> _deleteSubscription(BuildContext context) async {
     final db = Db();
-    // Confirm dialog
-    final confirm = await showDialog<bool>(
+
+    final confirm = await showDialog(
       context: context,
-      builder: (context) => AlertDialog(
-        title: const Text('Delete Subscription'),
-        content: const Text('Are you sure you want to delete this subscription?'),
-        actions: [
-          TextButton(onPressed: () => Navigator.pop(context, false), child: const Text('Cancel')),
-          TextButton(onPressed: () => Navigator.pop(context, true), child: const Text('Delete')),
-        ],
-      ),
+      builder:
+          (context) => AlertDialog(
+            title: const Text('Delete Subscription?'),
+            content: const Text(
+              'Are you sure you want to delete this subscription?',
+            ),
+            actions: [
+              TextButton(
+                onPressed: () => Navigator.pop(context, false),
+                child: const Text('Cancel'),
+              ),
+              TextButton(
+                onPressed: () => Navigator.pop(context, true),
+                child: const Text('Delete'),
+              ),
+            ],
+          ),
     );
     if (confirm != true) return;
-
     await db.deleteSubscription(subscriptionElement.id);
-    // Remove from notifier
-    final updatedList = List<Subscription>.from(subscriptionsNotifier.value)
-      ..removeWhere((s) => s.id  == subscriptionElement.id);
-    subscriptionsNotifier.value = updatedList;
-    ScaffoldMessenger.of(context).showSnackBar(const SnackBar(content: Text('Subscription deleted')));
+
+    final updatedList = List<Subscription>.from(subscriptionNotifier.value)
+      ..removeWhere((s) => s.id == subscriptionElement.id);
+    subscriptionNotifier.value = updatedList;
+    ScaffoldMessenger.of(
+      context,
+    ).showSnackBar(const SnackBar(content: Text('Subscription Deleted!')));
   }
 
   @override
@@ -72,7 +81,13 @@ class SubscriptionItem extends StatelessWidget {
             ),
           ],
         ),
-        trailing: IconButton(onPressed: () => _deleteSubscription(context), icon: Icon(Icons.delete, color: const Color.fromARGB(255, 205, 61, 61),)),
+        trailing: IconButton(
+          onPressed: () => _deleteSubscription(context),
+          icon: Icon(
+            Icons.delete,
+            color: const Color.fromARGB(255, 205, 61, 61),
+          ),
+        ),
       ),
     );
   }
