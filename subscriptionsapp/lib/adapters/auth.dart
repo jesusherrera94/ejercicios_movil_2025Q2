7d31f2c01fe7b7305fcfb6923eca960ed74b9d09
Jesus Herrera <jesusherrera1594@gmail.com>
2025-06-15 17:37:29 -0600

    complete all features

--- subscriptionsapp/lib/adapters/auth.dart
+++ subscriptionsapp/lib/adapters/auth.dart
@@ -4,4 +4,5 @@ class Auth {
   // no lo vamos a utilizar
   static dynamic createUserWithEmailAndPassword(String email, String password) async => await FirebaseAuth.instance.createUserWithEmailAndPassword(email: email, password: password);
   static dynamic signInWithEmailAndPassword(String email, String password) async => await FirebaseAuth.instance.signInWithEmailAndPassword(email: email, password: password);
+  static dynamic signOut() async => await FirebaseAuth.instance.signOut();
 }
--- subscriptionsapp/lib/utils/time_utils.dart
+++ subscriptionsapp/lib/utils/time_utils.dart
@@ -3,6 +3,7 @@ import 'package:intl/intl.dart';
 String timestampToDatestring(int unixTimestamp) {
     DateTime dateTime = DateTime.fromMillisecondsSinceEpoch(
       unixTimestamp * 1000,
+      isUtc: true,
     );
     DateFormat formatter = DateFormat('dd-MM-yyyy');
     String formattedDate = formatter.format(dateTime);
